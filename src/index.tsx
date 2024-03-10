b30bb6ec72f70d0424e4a8d94ce6b172c16e9c4d
Souvik Dey <souvik.dey@cloudkaptan.com>
2024-03-10 12:36:48 +0530

    Init

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@mui/material/styles'
 import './index.css';
-import App from './AppXX';
+import App from './App';
 import { lightTheme } from './utils/theme';
 import reportWebVitals from './reportWebVitals';
 
